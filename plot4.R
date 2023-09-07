c4ff04e4eb9d3b4e1cbdf346aa66c35401a592f0
Edgardo García <nomasopa@gmail.com>
2023-09-07 16:38:32 -0300

    Corrección en cuadro de leyenda.

--- plot4.R
+++ plot4.R
@@ -89,7 +89,7 @@ with(filter_ep, {
         with(filter_ep, lines(Time, Sub_metering_2, type = "l", col = "red"))
         with(filter_ep, lines(Time, Sub_metering_3, type = "l", col = "blue"))
         legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-               col = c("black", "red", "blue"), lty = 1)
+               col = c("black", "red", "blue"), lty = 1, bty = "n")
         plot(Time, Global_reactive_power,
              type = "l",
              xaxt = "n",
